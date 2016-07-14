c6d5afc209013967ad74641f97148d62e21e7843
VladimirValitski <valodik.valitski@gmail.com>
2016-07-14 22:22:38 +0300

    add Task4

---
+++ HomeWork4/Customer.java
@@ -0,0 +1,110 @@
+package homeWork4;
+
+/*Create class Customer with elements: id,surname,name,econdName,adress,
+ * numberCreditCard,numberBankCard*/
+public class Customer {
+	private int id;
+	private String surname;
+	private String name;
+	private String secondName;
+	private String adress;
+	private int numberCreditCard;
+	private int numberBankCard;
+	
+	
+	public Customer(int id,String surname,String name, String secondname, 
+			String adress,int numberCreditCard,int numberBankCard){
+		//create constructor
+		this.id = id;
+		this.surname = surname;
+		this.name = name;
+		this.secondName = secondname;
+		this.adress = adress;
+		this.numberCreditCard = numberCreditCard;
+		this.numberBankCard = numberBankCard;
+	}
+	
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+
+	public String getAdress() {
+		return adress;
+	}
+
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+
+	public int getNumberCreditCard() {
+		return numberCreditCard;
+	}
+
+
+	public void setNumberCreditCard(int numberCreditCard) {
+		this.numberCreditCard = numberCreditCard;
+	}
+
+
+	public int getNumberBankCard() {
+		return numberBankCard;
+	}
+
+
+	public void setNumberBankCard(int numberBankCard) {
+		this.numberBankCard = numberBankCard;
+	}
+
+	
+		
+		@Override
+        public String toString() // create @Override for change method toString() , for understanding output strings
+        {
+             return "Customer{ id: " + id + " " + surname + " " + name + " " + secondName + 
+            		 ", adress: " + adress + ", numberCreditCard: " + numberCreditCard +
+            		 ", numberBankCard: " + numberBankCard + "}";
+
+        }
+                
+
+	}
+
+   
+    
+
+
---
+++ HomeWork4/DataBase.java
@@ -0,0 +1,43 @@
+package homeWork4;
+
+public class DataBase {
+/* Creating customer databases*/
+	public static void main(String[] args) {
+		Customer [] customer = addCustomer(); //create  array customers
+		
+		System.out.println("Список покупателей в алфавитном порядке:");
+		//listAlphabet(customer);//sort customers by alphabet
+		System.out.println("Список покупателей, у которых номер кредитной карточки находится "
+				+ "в интервале(2000-3000):");
+		 listCreditCard(customer);//sort customers by CreditCard
+		
+   }
+	
+	private static Customer[] addCustomer(){
+		Customer [] customer = new Customer[5];//create method for array customers
+		 customer [0] = new Customer(1, "Valitski","Vladimir","Viktorovich","Minsk,Kalinina 12",2945,3456);
+		 customer [1] = new Customer(2, "Tartega","Vlad","Viktorovich","Minsk,Linina1",3545,3116);
+		 customer [2] = new Customer(3, "Farega","Dmitri","Karlovich","Minsk,Lenina 23",2224,1567);
+		 customer [3] = new Customer(4, "Dega","Alex","Dmitrich","Moscow,Lenina 45",2001,2007);
+		 customer [4] = new Customer(5, "Fers","Mihail","Egorovich","Minsk,Esenina 32",1004,2001);
+		return customer;
+	}
+	
+	/*private static void listAlphabet(Customer [] customer) {
+		for (Customer cus: customer){
+			if(){
+				System.out.println(cus);
+			}
+		}
+	}*/
+	
+private static void listCreditCard(Customer [] customer) {//create method for sort customers by CreditCard
+		for (Customer cus: customer){
+			if((cus.getNumberCreditCard() > 2000) && ((cus.getNumberCreditCard()< 3000))){
+				System.out.println(cus);
+			}
+		}
+
+	}
+
+}
