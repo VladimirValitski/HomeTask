90e6d25891f5dd70221b3b2d6d713ccbf63f467c
VladimirValitski <valodik.valitski@gmail.com>
2016-07-23 21:05:46 +0300

    add homeWork1 and del unused files

--- HomeWork1/Task1Hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Task1Hello {
-public static void main(String [] args) {
-System.out.print("Введите имя: ");
-Scanner scanner = new Scanner(System.in);
-String text = scanner.nextLine(); //Считываем строку целиком
-System.out.println("Привет, " + text + "!");
-}
-}
